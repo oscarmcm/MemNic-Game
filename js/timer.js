9c89fc6ce40bfb1ac32f418cca4390c6892c5043
Cajetero <om.cortez.2010@gmail.com>
2014-09-01 17:14:45 -0600

    timer function in pure javascript

---
+++ js/timer.js
@@ -0,0 +1,37 @@
+var time;
+var url = window.location.hash.substr(1);
+if (url == 1) {
+	time = 0;
+}
+else if (url == 2) {
+	time = 4;
+}
+else if (url == 3) {
+	time = 2;
+}
+else if (url == 4) {
+	time = 1;
+}
+
+function countdown(minutes) {
+	var seconds = 60;
+	var mins = minutes
+	function tick() {
+		var counter = document.getElementById("timer");
+		var current_minutes = mins-1
+		seconds--;
+		counter.innerHTML = 
+		current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+		if( seconds > 0 ) {
+			setTimeout(tick, 1000);
+		} else {
+
+			if(mins > 1){
+               setTimeout(function () { countdown(mins - 1); }, 1000);
+           }
+       }
+   }
+   tick();
+}
+
+countdown(time);
